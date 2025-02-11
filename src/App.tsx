a6cb839870591ba5cf0b73dc8bf0551131f96c4e
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2025-02-11 08:57:42 +0100

    refactor: use HeroesList in App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-import { useState } from 'react';
-import { Hero } from './types/hero';
-import { getHeroesByFirstLetter } from './api/heroes';
-
-const alphabet: string[] = [];
-
-for (let i = 65; i <= 90; i++) {
-  alphabet.push(String.fromCharCode(i));
-}
-
-// Fonction pure:
-// Pas d'effet de bord (no side effect)
-// Input donné, toujours le même output en sortie
-// Math.random()
+import HeroesList from './pages/HeroesList/HeroesList';
 
 function App() {
-  const [selectedLetter, setSelectedLetter] = useState('A');
-  const [heroes, setHeroes] = useState<Hero[]>([]);
-
-  const selectLetter = (letter: string) => {
-    setSelectedLetter(letter);
-    getHeroesByFirstLetter(letter).then((data) => {
-      setHeroes(data);
-    });
-  };
-
   return (
     <>
-      <h1>Hello World</h1>
-      <section>
-        {alphabet.map((letter) => (
-          <button
-            key={letter}
-            style={{
-              color: selectedLetter === letter ? 'red' : 'black',
-            }}
-            onClick={() => selectLetter(letter)}
-          >
-            {letter}
-          </button>
-        ))}
-        <div>
-          {heroes.map((hero) => (
-            <span key={hero.id}>{hero.name}</span>
-          ))}
-        </div>
-      </section>
+      <HeroesList />
     </>
   );
 }
